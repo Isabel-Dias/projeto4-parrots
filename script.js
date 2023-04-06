119c5070522f6a83eeaae13f3cea11d14c00d098
IsabelDias <isabel.c.andrade.dias@gmail.com>
2023-04-06 11:02:28 -0300

    feat: Seleção e Shuffle das cartas de acordo com o número selecionado

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+//Valid card number check
 let cardsNumber;
 let checker;
 
 askForCardNumber();
 checkNumber();
 
-//Valid card number check
 function askForCardNumber() {
-    cardsNumber = prompt('Com quantas cartas você quer jogar?');
-    Number(cardsNumber);
+    cardsNumber = Number(prompt('Com quantas cartas você quer jogar?'));
     if(cardsNumber <= 14 && cardsNumber >= 4 && cardsNumber % 2 == 0) {
         checker = true;
     }
@@ -17,5 +16,90 @@ function checkNumber() {
         askForCardNumber();
     }
 }
+
+//Card Selection
+let cardsIndex = (cardsNumber / 2) - 1;
+let selectedCards = [];
+
+function cardSelection(){
+    let i = 0;
+    while (i < cardsIndex) {
+        selectedCards.push(cardsContainer[i]);
+        selectedCards.push(cardsContainer[i]);
+        i++;
+    }
+}
+
+cardSelection()
+
+//Card Shuffle
+for (let i = selectedCards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [selectedCards[i], selectedCards[j]] = [selectedCards[j], selectedCards[i]];
+}
+
+//Card Library
+const cardsContainer = [
+    `<div class="card">
+        <div class="front-face face">
+        <img class="front-img" src="./images/back.png">
+        </div>
+        <div class="back-face face">
+        <img class="back-gif" src="./images/bobrossparrot.gif">
+        </div>
+    </div>`,
+    `<div class="card">
+        <div class="front-face face">
+        <img class="front-img" src="./images/back.png">
+        </div>
+        <div class="back-face face">
+        <img class="back-gif" src="./images/explodyparrot.gif">
+        </div>
+    </div>`,
+    `<div class="card">
+        <div class="front-face face">
+        <img class="front-img" src="./images/back.png">
+        </div>
+        <div class="back-face face">
+        <img class="back-gif" src="./images/fiestaparrot.gif">
+        </div>
+    </div>`,
+    `<div class="card">
+        <div class="front-face face">
+        <img class="front-img" src="./images/back.png">
+        </div>
+        <div class="back-face face">
+        <img class="back-gif" src="./images/metalparrot.gif">
+        </div>
+    </div>`,
+    `<div class="card">
+        <div class="front-face face">
+        <img class="front-img" src="./images/back.png">
+        </div>
+        <div class="back-face face">
+        <img class="back-gif" src="./images/revertitparrot">
+        </div>
+    </div>`,
+    `<div class="card">
+        <div class="front-face face">
+        <img class="front-img" src="./images/back.png">
+        </div>
+        <div class="back-face face">
+        <img class="back-gif" src="./images/tripletsparrot.gif">
+        </div>
+    </div>`,
+    `<div class="card">
+        <div class="front-face face">
+        <img class="front-img" src="./images/back.png">
+        </div>
+        <div class="back-face face">
+        <img class="back-gif" src="./images/unicornparrot.gif">
+        </div>
+    </div>`
+]
+
+
+
+
 
 
